08df08be65556ae8d15de1c10a299e501acfb07c
hiran-shalika <hrianslfd99@gmail.com>
2024-07-08 07:38:06 +0530

    update

---
+++ script.js
@@ -0,0 +1,7 @@
+// ad-script.js - shortpins ads
+(function() {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = '//pl22417838.highrevenuenetwork.com/83/12/f8/8312f879f3b66c46ffa1d1d8cebc1362.js';
+    document.head.appendChild(script);
+})();
